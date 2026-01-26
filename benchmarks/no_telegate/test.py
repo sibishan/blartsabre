5d378cbf4dc6ae821bf49dd187bea6e2a39b2bb1
Sibishan R <54585836+sibishan@users.noreply.github.com>
2026-01-26 01:09:13 +0000

    added 9 qubit circuit to test

--- benchmarks/no_telegate/test.py
+++ benchmarks/no_telegate/test.py
@@ -9,26 +9,26 @@ from .telesabre import run_telesabre
 
 config = Config()
 
-qc = qasm2.load("./data/example_4q/example_4q.qasm")
+qc = qasm2.load("./data/example_9q/example_9q.qasm")
 cir = Circuit.from_qiskit(qc)
 print(cir)
 
-num_qubits = 12
-qubit_to_core = [0,0,0,0, 1,1,1,1, 2,2,2,2]
+num_qubits = 15
+qubit_to_core = [0,0,0,0,0, 1,1,1,1,1, 2,2,2,2,2]
 
-# core 0: line 0-1-2-3
-edges0 = [Edge(0,1), Edge(1,2), Edge(2,3)]
-# core 1: star centred at 4
-edges1 = [Edge(4,5), Edge(4,6), Edge(4,7)]
+# core 0: line 0-1-2-3-4
+edges0 = [Edge(0,1), Edge(1,2), Edge(2,3), Edge(3,4)]
+# core 1: star centred at 5
+edges1 = [Edge(5,6), Edge(5,7), Edge(5,8), Edge(5,9)]
 # core 2: ring
-edges2 = [Edge(8,9), Edge(9,10), Edge(10,11), Edge(11,8)]
+edges2 = [Edge(10,11), Edge(11,12), Edge(12,13), Edge(13,14), Edge(14,10)]
 
 intra_core_edges = edges0 + edges1 + edges2
 
 # inter core links between communication qubits
 inter_core_edges = [
-    Edge(2,4), # core0 comm qubit 2 <-> core1 comm qubit 4
-    Edge(3,8) # core0 comm qubit 3 <-> core1 comm qubit 8
+    Edge(2,5), # core0 comm qubit 2 <-> core1 comm qubit 4
+    Edge(3,10) # core0 comm qubit 3 <-> core1 comm qubit 8
 ]
 
 arch = Architecture(
--- tests/distributed_test.py
+++ tests/distributed_test.py
@@ -4,50 +4,34 @@ from router.sabre import sabre_swap
 from architecture import *
 from qiskit import QuantumCircuit
 from convert import from_qiskit
-import qiskit.qasm2
+from qiskit import qasm2
 
-# qc = qiskit.qasm2.load("./data/quekno/20Q_depth_Tokyo/20QBT_depth_Tokyo_large_opt_1_1.5_no.0.qasm")
+qc = qasm2.load("./data/example_9q/example_9q.qasm")
 
-num_rows = 4
-qc = QuantumCircuit(2 * num_rows, 2 * num_rows)
-for i in range(num_rows):
-    qc.h(i)
-    qc.cx(i, i + num_rows)
-    qc.measure(i, i)
-    qc.measure(i + num_rows, i + num_rows)
+arch = DistributedQubitNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),
+                                                         (8,9),(8,10),(9,11),(10,11),(12,13),(12,14),(13,15),(14,15),
+                                                         (1,4),(2,8),(7,13),(11,14)],
+                                                     core_node_groups=[[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
 
-# arch = DistributedQubitNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),
-#                                                          (8,9),(8,10),(9,11),(10,11),(12,13),(12,14),(13,15),(14,15),
-#                                                          (1,4),(2,8),(7,13),(11,14)],
-#                                                      core_node_groups=[[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
-# # arch = DistributedQubitNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),(1,6),(3,4),(1,4),(3,6)],
-# #                                                     core_node_groups=[[0,1,2,3],[4,5,6,7]])
-# arch.draw()
+initial_mapping = telesabre(arch, qc, verbose=True)
 
-## TElesabre test
-# initial_mapping = telesabre(arch, qc, verbose=True)
+print(initial_mapping)
 
 # Prof's idea test
 
-arch = SingleCoreDQGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),
-                                                         (8,9),(8,10),(9,11),(10,11),(12,13),(12,14),(13,15),(14,15),
-                                                         (1,4),(2,8),(7,13),(11,14)],
-                                                     name="Distributed Qubit Graph with Single Core",
-                                                     comm_edges=[(1,4),(2,8),(7,13),(11,14)])
-# arch = DistributedQubitNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),(1,6),(3,4),(1,4),(3,6)],
-#                                                     core_node_groups=[[0,1,2,3],[4,5,6,7]])
-# arch.draw()
+# arch = SingleCoreDQGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),
+#                                                          (8,9),(8,10),(9,11),(10,11),(12,13),(12,14),(13,15),(14,15),
+#                                                          (1,4),(2,8),(7,13),(11,14)],
+#                                                      name="Distributed Qubit Graph with Single Core",
+#                                                      comm_edges=[(1,4),(2,8),(7,13),(11,14)])
+# # arch = DistributedQubitNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7),(1,6),(3,4),(1,4),(3,6)],
+# #                                                     core_node_groups=[[0,1,2,3],[4,5,6,7]])
+# # arch.draw()
 
-initial_mapping = sabre(arch, qc, verbose=True)
-
-routed_qc, mapping, log = sabre_swap(arch, qc, initial_mapping)
-# print(arch.distance_matrix)
-print(routed_qc)
-print(mapping)
-print(log)
+# initial_mapping = sabre(arch, qc, verbose=True)
 
 # routed_qc, mapping, log = sabre_swap(arch, qc, initial_mapping)
-
+# # print(arch.distance_matrix)
 # print(routed_qc)
 # print(mapping)
 # print(log)
