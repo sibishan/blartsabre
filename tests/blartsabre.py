a39ce9a05c8bbec85eca47cda7434b598132a760
ExaMegaByte <Elan.M.Blaustein@student.uts.edu.au>
2026-03-08 01:38:35 +1100

    None fix

--- tests/blartsabre.py
+++ tests/blartsabre.py
@@ -4,14 +4,16 @@ from qiskit import QuantumCircuit
 from convert import from_qiskit
 import qiskit.qasm2
 
-qc = qiskit.qasm2.load("./data/quekno/53Q_depth_Rochester/53QBT_depth_Rochester_large_opt_1_1.5_no.0.qasm")
+# qc = qiskit.qasm2.load("./data/quekno/53Q_depth_Rochester/53QBT_depth_Rochester_large_opt_1_1.5_no.0.qasm")
+qc = QuantumCircuit.from_qasm_file("./data/telesabre/qasm_25/graphstate_nativegates_ibm_qiskit_opt3_25.qasm")
 
 # arch = BLARTNetworkGraph([(0,1),(0,2),(1,3),(2,3),(4,5),(4,6),(5,7),(6,7)], blart_edge_groups=[([1,3],[4,6])])
 # arch = BLARTNetworkGraph([(0,1),(0,2),(1,3),(2,3),(0,8),(2,8),(4,5),(4,6),(5,7),(6,7),(5,9),(7,9)], blart_edge_groups=[([1,3],[4,6])])
-arch = blart_four_tokyo()
+arch = blart_grid(4,4,2,3)
+arch = blart_grid(3,3,2,2)
 arch.draw()
 
-# initial_mapping = blartsabre_layout(arch, qc, verbose=True, num_iterations=10)
+initial_mapping = blartsabre_layout(arch, qc, verbose=True, num_iterations=10)
 
 # routed_qc, mapping, log = sabre_swap(arch, qc, initial_mapping)
 # print(arch.distance_matrix)
